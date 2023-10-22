ce38a30be33d2c0b6b54acef590b09d8e3331bba
MariCarmen-li <73373950+MariCarmen-li@users.noreply.github.com>
2023-10-21 22:35:51 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import ItemListContainer from './components/ItemListContainer';
+import CartWidget from './components/CartWidget';
 
 const App = () => {
+  const [cartItemCount, setCartItemCount] = useState(0);
+
+  // Función para aumentar la cantidad de productos en el carrito
+  const addToCart = (count) => {
+    setCartItemCount(cartItemCount + count);
+  };
+
   return (
     <Router>
       <div className="App">
         <Navbar />
+        <CartWidget itemCount={cartItemCount} />
         <Switch>
-          <Route path="/category/:id" component={ItemListContainer} />
-          <Route path="/item/:id" component={ItemListContainer} />
-          <Route path="/" component={ItemListContainer} />
+          <Route
+            path="/category/:id"
+            render={(props) => (
+              <ItemListContainer {...props} addToCart={addToCart} />
+            )}
+          />
+          <Route
+            path="/item/:id"
+            render={(props) => (
+              <ItemListContainer {...props} addToCart={addToCart} />
+            )}
+          />
+          <Route
+            exact
+            path="/"
+            render={(props) => (
+              <ItemListContainer {...props} addToCart={addToCart} />
+            )}
+          />
         </Switch>
       </div>
     </Router>
