85fecce3f9f6db2ca59ad6d82d7d7de21beac749
MariCarmen-li <73373950+MariCarmen-li@users.noreply.github.com>
2023-10-21 22:33:54 -0600

    Update index.jsx

--- src/mocks/index.jsx
+++ src/mocks/index.jsx
@@ -1,30 +1,4 @@
-/*
-import productsData from '../data/products.json';
-import categoriesData from '../data/categories.json';
 
-export const fetchProducts = (categoryId) => {
-  // Simulamos una llamada API para obtener productos filtrados por categoría
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      if (categoryId) {
-        const filteredProducts = productsData.filter((product) => product.categoryId === categoryId);
-        resolve(filteredProducts);
-      } else {
-        resolve(productsData);
-      }
-    }, 1000);
-  });
-};
-
-export const fetchCategories = () => {
-  // Simulamos una llamada API para obtener la lista de categorías
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(categoriesData);
-    }, 1000);
-  });
-};
-*/
 import productsData from '../data/products.json';
 import categoriesData from '../data/categories.json';
 
